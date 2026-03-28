da2a763d37c4e7b776d14c74671c75e994714b23
Dariel <Dariel@DESKTOP-2MA4V0O>
2026-03-28 16:24:35 -0600

    Corrección Semana 9

---
+++ src/main/java/com/tienda/repository/ProductoRepository.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.repository;
+
+import com.tienda.domain.Producto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/**
+ *
+ * @author Elioth Artavia
+ */
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+    public List<Producto> findByActivoTrue();
+    
+    public List<Producto> findByPrecioBetweenOrderByPrecioAsc(double precionInf, double precioSup);
+    
+    
+    @Query(value = "SELECT p FROM Producto p WHERE p.precio BETWEEN :precioInf AND :precioSup ORDER BY p.precio ASC")
+    public List<Producto> consultaJPQL(@Param("precioInf") double precioInf, @Param("precioSup") double precioSup);
+    
+    
+    
+    
+    @Query(nativeQuery = true,
+            value = "SELECT * FROM producto p WHERE p.precio BETWEEN :precioInf AND :precioSup ORDER BY p.precio ASC")
+    public List<Producto> consultaSQL(@Param("precioInf") double precionInf, @Param("precioSup")double precioSup);
+    
+    
+    
+    
+    
+    
+}
